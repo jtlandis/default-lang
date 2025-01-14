98218ff527ace2b51df3e8a04da3af255d094858
jtlandis <jtlandis314@gmail.com>
2025-01-14 10:44:15 -0500

    fixing bug in which cell-output blocks were syntaxed highlighted

--- _extensions/default-lang/default-lang.lua
+++ _extensions/default-lang/default-lang.lua
@@ -7,12 +7,48 @@ function has_any_class(el)
   return false
 end
 
+function has_class(el, class)
+  if has_any_class(el) then
+    for _, c in pairs(el.classes) do
+      if c == class then
+        return true
+      end
+    end
+  end
+  return false
+end
+
 function add_lang(el)
   if not has_any_class(el) then
     -- el has not class, thus we can add the lang
-    el.classes = { lang[1].text }
+    el.classes = { lang }
   end
   return el
+end
+
+cleanup_cell_output = {
+  Div = function(el)
+    if has_class(el, "cell-output") then
+      for i, block in ipairs(el.content) do
+        if has_class(block, lang) then
+          for ii, c in ipairs(block.classes) do
+            if c == lang then
+              table.remove(block.classes, ii)
+              break
+            end
+          end
+        end
+      end
+    end
+    return el
+  end
+}
+
+function parse_lang(lang)
+  if lang[1].t ~= "Str" then
+    error("default-code-lang must be a string")
+  end
+  return lang[1].text
 end
 
 default_lang = {
@@ -22,8 +58,9 @@ default_lang = {
 
 function Pandoc(doc)
   if doc.meta['default-code-lang'] then
-    lang = doc.meta['default-code-lang']
+    lang = parse_lang(doc.meta['default-code-lang'])
     local blocks = pandoc.walk_block(pandoc.Div(doc.blocks), default_lang)
+    blocks = pandoc.walk_block(blocks, cleanup_cell_output)
     doc.blocks = blocks.content
   end
 
